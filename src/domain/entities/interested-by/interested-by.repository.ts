84782f02ede0dd60da6e33036387dbee6e9c0836
Shariq Munir <shariq.munir7@gmail.com>
2024-11-18 08:03:04 +0500

    update-interstedBy-delete-functionality

--- src/domain/entities/interested-by/interested-by.repository.ts
+++ src/domain/entities/interested-by/interested-by.repository.ts
@@ -2,7 +2,7 @@ import { InterestedBy, SerializedInterestedBy } from './interested-by.entity';
 
 export abstract class InterestedByRepository {
   abstract insert(entity: InterestedBy);
-  abstract delete(id: string);
+  abstract delete(userId: string, eventId: string);
   abstract fetchByEventId(id: string);
   abstract fetchByUserId(id: string): Promise<SerializedInterestedBy[]>;
 }
