ee7bdf49718f98e28998b8c6da2e63d2beea7b3b
Shariq Munir <shariq.munir7@gmail.com>
2024-11-24 19:34:08 +0500

    Add cors option

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(WebModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If using cookies or authorization headers
+    allowedHeaders: 'Authorization, Content-Type', // Allow Authorization header
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
